1b3f955b2766aef3cc839df84951ba051060e863
PersonCollection <verne_ad@163.com>
2019-03-07 15:17:17 +0800

    Update java of File Header.java

--- fileTemplates/includes/java of File Header.java
+++ fileTemplates/includes/java of File Header.java
@@ -3,6 +3,6 @@
 * @author: zuoweichen
 * @date: ${DATE}
 * @time: ${TIME}
-* Copyright (C) 2018 MTDP
+* Copyright (C) 2018 
 * All rights reserved                                                  
 */
